f53d2cdd3c5c9e042ba08b835144510e036aed76
sergioetrindade <sergio.trindade@bandtec.com.br>
2020-09-18 12:14:53 -0300

    Modificações 18/09/2020

--- Banco-de-Dados/tabelasprint1.sql
+++ Banco-de-Dados/tabelasprint1.sql
@@ -1,6 +1,8 @@
 create database Projeto;
 
+
 use projeto;
+
 
 create table usuario (
 	idusuario int primary key auto_increment,
@@ -17,14 +19,19 @@ create table sensores (
 	Temporeal datetime
 );
 insert sensores (temperatura,Temporeal) values 
-(5,'2020-09-15 10:39:55');
+(-10,'2020-09-15 10:39:25'),
+(-13,'2020-09-15 11:39:45'),
+(-18,'2020-09-15 14:24:35'),
+(-14,'2020-09-15 17:32:20'),
+(-11,'2020-09-15 11:20:17');
 
 insert into usuario (nome,telefone,email,dataNasc) values 
-('Matheus Alencar', '40028922', 'matheus.alencar@gmail.com','1997-08-27'),
-('DJ Rogerinho', '40028662', 'dj.rogerinho@gmail.com','1994-02-07'),
-('Priscila TeyTey', '89987070', 'apriscilinha.mandrake@gmail.com','2001-10-20'),
-('Jéssica armando', '940227749', 'jessiquinha.vidaloka@gmail.com','1998-02-19');
-
+('Sergio Trindade', '40028922', 'sergio.trindade@bandtec.com.br','1997-08-27'),
+('João Arnaut', '40028662', 'joao.silva@bandtec.com.br','1994-02-07'),
+('Lucas Penalva', '89987070', 'lucas.pfranca@bandtec.com.br','2001-10-20'),
+('Rafael Rocha', '940227749', 'rafael.rocha@bandtec.com.br','1998-02-19'),
+('Marcus Vinicius','943434754','marcus.vinicius@bandtec.com.br','1998-01-07');
 select * from usuario;
 
 select * from sensores;
+ 
